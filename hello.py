3ede4099ec8a7873411b2bb3f81aca28b3f22163
Vitoria <vitoriac.benedet@gmail.com>
2025-05-17 13:19:36 -0300

    gerar compl

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
-with open('/home/users/vcbenedet/FM30k/hello.txt', 'w') as f:
-    f.write("Hello World!!!\n")
+import torch
+import pickle
+import clip
+from torch.utils.data import Dataset, DataLoader
+import os
+from PIL import Image
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm import tqdm
+import pandas as pd
