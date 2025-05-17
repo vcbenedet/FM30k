94d3e48605f240e423b00097af7c74f07baf3c27
Vitoria Colonetti Benedet <vitoriacbenedet@gmail.com>
2025-05-17 20:14:15 -0300

    fix. log

--- extrair.py
+++ extrair.py
@@ -13,6 +13,7 @@ with tarfile.open(tar_gz_path, 'r:gz') as tar:
     tar.extractall(path=project_folder)
 
 # Verificar os arquivos extraídos
-extracted_files = os.listdir(project_folder)
-print(f"Quantidade arquivos extraídos: {len(project_folder)}")  
-print(f"Arquivos extraídos: {project_folder[:5]}") # Mostrar os primeiros 5 arquivos extraídos
+extracted_files_path = os.path.join(project_folder, 'flickr30k-images')
+extracted_files = os.listdir(extracted_files_path)
+print(f"Quantidade arquivos extraídos: {len(extracted_files)}")  
+print(f"Arquivos extraídos: {extracted_files[:5]}") # Mostrar os primeiros 5 arquivos extraídos
